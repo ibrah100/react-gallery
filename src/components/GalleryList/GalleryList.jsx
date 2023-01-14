abef830a0788678233ed16a53cbce7d65db50b10
ibrah100 <37915317+ibrah100@users.noreply.github.com>
2023-01-13 19:03:33 -0600

    finished base mode, have to refactor into GalleryList

--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,21 +1,42 @@
 import GalleryItem from "../GalleryItem/GalleryItem";
+import { useState } from "react";
 
 function GalleryList({ galleryList }){
+    const [isHover, setIsHover] = useState(false);
 
-    return (
-        <>
-        <ul>
-            {galleryList.map((image) => (
-                <div>
-                    <img src={image.path} width="300" height="300"></img>
-                    <button>💕</button>
-                    <h2>{image.likes} people like this!</h2>
-                </div>
-                // <GalleryItem image={image.path} key={image.id} />
-            ))}
-        </ul>
-        </>
-    )
+    const toggleHover = () => {
+        setIsHover(!isHover);
+    }
+
+    if (isHover === false) {
+        return (
+            <>
+            <ul>
+                {galleryList.map((image) => (
+                    <div>
+                        <img onClick={toggleHover} src={image.path} width="300" height="300"></img>
+                        <button>💕</button>
+                        <h2>{image.likes} people like this!</h2>
+                    </div>
+                    // <GalleryItem image={image.path} key={image.id} />
+                ))}
+            </ul>
+            </>
+        )
+    } else {
+        return (
+            <ul>
+                {galleryList.map((image) => (
+                    <div>
+                        <p onClick={toggleHover}>{image.description}</p>
+                        <button>💕</button>
+                        <h2>{image.likes} people like this!</h2>
+                    </div>
+                    // <GalleryItem image={image.path} key={image.id} />
+                ))}
+            </ul>
+        )
+    }
 }
 
 export default GalleryList;
